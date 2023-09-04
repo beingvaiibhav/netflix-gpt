de9e89cd11e02a394521474ab6f9ff5ac0f23e56
beingvaiibhav <vaibhavdubey1419@gmail.com>
2023-09-04 09:30:27 +0530

    Created Redux Store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <div className="App">
+      <Provider store={appStore}>
       <Body/>
+      </Provider>
     </div>
   );
 }
